924ea4300d350ce94c30c83117756ef8fdb0a94e
SVLOGer <max443584@gmail.com>
2025-12-07 18:16:07 +0300

    addtask

--- frontend/src/widgets/addTask/AddTask.tsx
+++ frontend/src/widgets/addTask/AddTask.tsx
@@ -1,9 +1,10 @@
 import {Button, Modal, Typography} from "antd";
 import styles from "./AddTask.module.css";
 import {TaskAppoint} from "../taskAppoint/TaskAppoint.tsx";
-import React from "react";
+import React, {useState} from "react";
 import type {TaskData} from "../../shared/types";
 import {useAppSelector} from "../../shared/hooks";
+import {TaskRedact} from "../taskRedact/TaskRedact.tsx";
 
 interface AddTaskProps {
     tasks: TaskData[]
@@ -18,41 +19,50 @@ const AddTask: React.FC<AddTaskProps> = ({
                                              closeAddTask,
                                              employerId,
 }) => {
+    const [isOpenAddTask, setIsOpenAddTask] = useState(false)
+
     const {users} = useAppSelector(state => state.users)
 
     const employer = users.find(user => user.id === employerId)
 
     const handleAddTask = () => {
-        console.log('add')
+        setIsOpenAddTask(true)
+    }
+
+    const handleCloseAddTask = () => {
+        setIsOpenAddTask(false)
     }
 
     return (
-        <Modal
-            title={<Typography.Title level={2} style={{textAlign: 'center'}}>Назначенные задачи</Typography.Title>}
-            open={isAddTaskOpen}
-            onCancel={closeAddTask}
-            footer={null}
-            width={1000}
-            style={{
-                top: '20px',
-            }}
-        >
-            <div className={styles.tasksContainer}>
-                <Button
-                    type='primary'
-                    size='large'
-                    style={{fontSize: '18px'}}
-                    onClick={handleAddTask}
-                >
-                    Добавить задачу
-                </Button>
-                <div className={styles.tasks}>
-                    {tasks.map((task) => (
-                        <TaskAppoint task={task} employer={employer}/>
-                    ))}
+        <>
+            <Modal
+                title={<Typography.Title level={2} style={{textAlign: 'center'}}>Назначение задачи</Typography.Title>}
+                open={isAddTaskOpen}
+                onCancel={closeAddTask}
+                footer={null}
+                width={1000}
+                style={{
+                    top: '20px',
+                }}
+            >
+                <div className={styles.tasksContainer}>
+                    <Button
+                        type='primary'
+                        size='large'
+                        style={{fontSize: '18px'}}
+                        onClick={handleAddTask}
+                    >
+                        Добавить задачу
+                    </Button>
+                    <div className={styles.tasks}>
+                        {tasks.map((task) => (
+                            <TaskAppoint task={task} employer={employer}/>
+                        ))}
+                    </div>
                 </div>
-            </div>
-        </Modal>
+            </Modal>
+            <TaskRedact isRedact={isOpenAddTask} closeRedact={handleCloseAddTask}/>
+        </>
     )
 }
 
